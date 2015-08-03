9c0181f2013ca34d9ae32b32b0d838d88119c97f
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2015-08-03 01:04:48 -0300

    Add index into quiz_controllers. Refactoring quiz_controller. Add show and answer in quiz_controller. Add another quiz into models seed. Add routes index, show and answer. Fix layout. Add index and show view.

--- models/models.js
+++ models/models.js
@@ -38,6 +38,10 @@ sequelize.sync().success(function() {
   Quiz.count().success(function(count) {
     if(count === 0) {
       Quiz.create({
+        pregunta: 'Capital de Portugal',
+        respuesta: 'Lisboa'
+      });
+      Quiz.create({
         pregunta: 'Capital de Italia',
         respuesta: 'Roma'}).success(function() {
           console.log('Base de datos Inicializada.')
