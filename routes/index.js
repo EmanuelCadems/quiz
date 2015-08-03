130f7ca624d39e8377aeed0d0af2aaa706c1adcc
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2015-08-03 01:56:14 -0300

    Refactoring DRY in quiz_controller with load method. Add route for load resource. Fix views: Answer and show

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Quiz' });
 });
 
+router.param('quizId', quizController.load);
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
