269e534afc2c1555b26af6443cc02524b8ee2a91
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2015-08-20 03:49:48 -0300

    Add login, logout, session_controller and user_controller

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-var quizController = require('../controllers/quiz_controller');
+var quizController    = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +14,10 @@ router.get('/author', function(req, res) {
 });
 
 router.param('quizId', quizController.load);
+
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
