25bb92db9e3208bb293ee09d3d4900eadb6ebfda
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2015-08-20 22:46:01 -0300

    Fix statistic controller for avoid timeout request in heroku

--- controllers/statistic_controller.js
+++ controllers/statistic_controller.js
@@ -11,17 +11,31 @@ var models = require('../models/models.js');
 
 
 exports.statistic = function(req, res) {
+  var commented_quizes = 0;
+  var no_commented     = 0;
   Promise.all(
     [
       models.Quiz.count(),
       models.Comment.count(),
-      models.Comment.findAndCountAll({ group: 'quizId', include: [{model: models.Quiz}] })
+      models.Quiz.findAll({
+        include: [
+          {model: models.Comment}
+        ]
+      })
+      // I have to use this because heroku through timeout with: models.Comment.findAndCountAll({ group: 'quizId', include: [{model: models.Quiz}] })
     ]
     ).then(function(results) {
+      for (var i in results[2]) {
+        if (results[2][i].comments.length) {
+            commented_quizes++;
+        } else {
+            no_commented++;
+        }
+      };
       res.render('statistics/statistic.ejs', {
           total_quizes: results[0],
           total_comments: results[1],
-          question_with_comments: results[2].rows.length,
+          question_with_comments: commented_quizes, //results[2].rows.length,
           errors: []});
     });
 }
